9132f2baf198bdd593f016335f6ac7819a295db8
Emon <emonwordpress.1000@gmaill.com>
2022-03-01 03:50:32 -0500

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-form v-model="valid">
+    <v-container>
+      <v-col cols="12" md="10">
+        <v-text-field
+          class="center"
+          v-model="firstname"
+          :rules="nameRules"
+          :counter="10"
+          label="First name"
+          required
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="12" md="10">
+        <v-text-field
+          class="center"
+          v-model="lastname"
+          :rules="nameRules"
+          :counter="10"
+          label="Last name"
+          required
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="12" md="10">
+        <v-text-field
+          class="center"
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
+      </v-col>
+
+      <v-col class="fil-center" cols="12" md="8">
+        <v-file-input accept="image/*" label="File input"></v-file-input>
+      </v-col>
+      <v-btn class="cen" depressed> Normal </v-btn>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style scoped>
+.center {
+  margin-left: 30%;
+}
+.fil-center {
+  margin-left: 22%;
+  width: 62%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import "../node_modules/vuetify/dist/vuetify.min.css";
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
